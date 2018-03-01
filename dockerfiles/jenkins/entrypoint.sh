968f1885236f4a715cf431dc068a960df8d7f20c
Guillermo Noain <guillermo.noain@quantion.com>
2018-03-01 12:33:47 +0100

    asdfasdf

--- dockerfiles/jenkins/entrypoint.sh
+++ dockerfiles/jenkins/entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-
-if [ -S ${DOCKER_SOCKET} ]; then
-    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
-    echo $DOCKER_GID
-    groupadd -for -g ${DOCKER_GID} dockerhost
-    usermod -aG dockerhost jenkins
-fi
+# echo $PATH
+# echo ${DOCKER_SOCKET}
+# if [ -S ${DOCKER_SOCKET} ]; then
+DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
+echo $DOCKER_GID
+groupadd -for -g ${DOCKER_GID} dockerhost
+usermod -aG dockerhost jenkins
+# fi
 
 exec sudo -E -H -u jenkins bash -c /usr/local/bin/jenkins.sh
